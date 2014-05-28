5a5af23daf91e632e13942f0ff2f378d4192308c
pascalc <pascal.chevrel@free.fr>
2014-05-28 19:15:43 +0200

    add Japanese

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Europe/Paris');
 include __DIR__ .'/functions.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
-$locales = ['de', 'es-ES', 'fr', 'id', 'it', 'pt-BR'];
+$locales = ['de', 'es-ES', 'fr', 'id', 'it', 'ja', 'pt-BR'];
 
 $addons = [
     'worldcupfeed' => '',
