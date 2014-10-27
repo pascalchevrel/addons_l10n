15dce5d5a20dc09eb553466af81b50bcbfca9e77
pascalc <pascal.chevrel@free.fr>
2014-10-27 14:03:55 +0100

    updated dtd/properties template

--- templates.php
+++ templates.php
@@ -89,28 +89,28 @@ prefs.cookies.firstPartyOnly={privacy_coach_properties->getStringNoHTML('Enabled
 prefs.enabled={privacy_coach_properties->getStringNoHTML('Enabled')}
 prefs.disabled={privacy_coach_properties->getStringNoHTML('Disabled')}
 
-prefs.currentValue={privacy_coach_properties->getStringNoHTML('Current value: %S')}
+prefs.currentValue={privacy_coach_properties->getStringNoHTML('Current setting: %S')}
 
-search.https={privacy_coach_properties->getStringNoHTML("Your default search engine (%S) uses HTTPS, so you're already secure.")}
-search.http={privacy_coach_properties->getStringNoHTML("Your default search engine (%S) doesn't use HTTPS, so you may want to change your default now.")}
+search.https={privacy_coach_properties->getStringNoHTML("Your default search engine (%S) uses HTTPS. Search away!")}
+search.http={privacy_coach_properties->getStringNoHTML("Your default search engine (%S) doesn't use HTTPS — you should consider changing it now.")}
 
 # https://support.mozilla.org/kb/clear-your-browsing-history-and-other-personal-dat
-banner.1.text={privacy_coach_properties->getStringNoHTML('Learn the different ways you can clear personal data stored in Firefox.')}
+banner.1.text={privacy_coach_properties->getStringNoHTML('Discover all the ways you can clear your personal browsing data from Firefox »')}
 
 # https://support.mozilla.org/kb/create-secure-passwords-keep-your-identity-safe
-banner.2.text={privacy_coach_properties->getStringNoHTML('Learn how to create secure, easy-to-remember passwords to keep your identity safe on the internet.')}
+banner.2.text={privacy_coach_properties->getStringNoHTML('Learn how to create secure, easy-to-remember passwords to keep your online identity safe »')}
 
 # https://support.mozilla.org/kb/firefox-health-report-understand-your-android-browser-perf
-banner.3.text={privacy_coach_properties->getStringNoHTML('Learn how Firefox Health Report lets you know how well your browser is performing and what steps you can take to improve it.')}
+banner.3.text={privacy_coach_properties->getStringNoHTML('Firefox Health Report determines how well your browser is performing and what you can do to improve it. Learn more »')}
 
 # https://support.mozilla.org/kb/how-does-insecure-content-affect-safety-android
-banner.4.text={privacy_coach_properties->getStringNoHTML('Learn how Firefox for Android automatically blocks insecure or mixed content from secure web pages.')}
+banner.4.text={privacy_coach_properties->getStringNoHTML('Discover how Firefox for Android automatically blocks unsecure or mixed content from otherwise secure Web pages »')}
 
 # https://support.mozilla.org/kb/share-your-android-device-firefox-guest-session
-banner.5.text={privacy_coach_properties->getStringNoHTML('Learn how to create a Guest Session to let someone else use Firefox without giving them access to your personal information.')}
+banner.5.text={privacy_coach_properties->getStringNoHTML('A guest session allows someone else to use your Firefox without giving them access to your personal information. Learn more »')}
 
 # https://support.mozilla.org/kb/mobile-private-browsing-browse-web-without-saving-syncing-info
-banner.6.text={privacy_coach_properties->getStringNoHTML("Learn how to create private tabs to browse the internet without saving any information about what pages you've visited.")}
+banner.6.text={privacy_coach_properties->getStringNoHTML('Browse the Internet without having any of your personal browsing data or history being saved. Learn more »')}
 
 TEMPLATE;
 };
@@ -120,63 +120,72 @@ $privacy_coach_dtd_template = function() use($privacy_coach_dtd) {
 <!ENTITY welcome.firefoxPrivacyCoach    "{$privacy_coach_dtd->getStringNoHTML('Firefox Privacy Coach')}">
 <!ENTITY welcome.byMozilla              "{$privacy_coach_dtd->getStringNoHTML('by Mozilla')}">
 
-<!ENTITY welcome.intro                  "{$privacy_coach_dtd->getStringNoHTML('Welcome to the Firefox Privacy Coach! This add-on will teach you about the privacy features that Firefox has to offer.')}">
+<!ENTITY welcome.intro                  "{$privacy_coach_dtd->getStringNoHTML('Welcome to Privacy Coach, your one-stop shop to learn about and manage your Firefox for Android privacy settings. You can revisit this page at any time by opening the Tools menu in your browser.')}">
 
 <!ENTITY prefs.title                    "{$privacy_coach_dtd->getStringNoHTML('Privacy Preferences')}">
-<!ENTITY prefs.intro                    "{$privacy_coach_dtd->getStringNoHTML("Firefox allows you to customize preferences that affect your privacy. We've broken down this list of preferences to explain what they actually mean and help you figure out what setting is right for you.")}">
+<!ENTITY prefs.intro                    "{$privacy_coach_dtd->getStringNoHTML('Firefox allows you to customize various settings in an effort to make your browsing experience as private and secure as possible. We’ve broken down this list of preferences to explain what they do and to help you figure out which settings are right for you.')}">
 
 <!ENTITY prefs.dnt.name                 "{$privacy_coach_dtd->getStringNoHTML('Do Not Track')}">
-<!ENTITY prefs.dnt.desc                 "{$privacy_coach_dtd->getStringNoHTML("Most major websites track their visitors' behavior and then sell or provide that information to other companies (like advertisers). Firefox has a Do Not Track feature that lets you tell websites you don't want your browsing behavior tracked.")}">
+<!ENTITY prefs.dnt.desc                 "{$privacy_coach_dtd->getStringNoHTML('This Firefox innovation helps you stay in control of how your browsing information is collected and used online. When the feature is enabled, Firefox will tell advertising networks, websites and applications that you want to opt out of tracking for purposes like behavioral advertising.')}">
+<!ENTITY prefs.dnt.learnMore            "{$privacy_coach_dtd->getStringNoHTML('Learn more about Do Not Track')}">
+
 <!ENTITY prefs.cookies.name             "{$privacy_coach_dtd->getStringNoHTML('Cookies')}">
-<!ENTITY prefs.cookies.desc             "{$privacy_coach_dtd->getStringNoHTML('A cookie is information stored on your computer by a website you visit. Third-party cookies are cookies that are set by a website other than the one you are currently on. Some advertisers use these types of cookies to track your visits to the various websites on which they advertise.')}">
+<!ENTITY prefs.cookies.desc             "{$privacy_coach_dtd->getStringNoHTML('A cookie is information stored on your computer about the websites you visit. Third-party cookies are those set by a website other than the one you are currently on. Some advertisers use these types of cookies to track your visits to the websites on which they advertise. Firefox has settings that allow you to approve or deny cookie storage requests, delete cookies automatically and more.')}">
+<!ENTITY prefs.cookies.learnMore        "{$privacy_coach_dtd->getStringNoHTML('Learn more about Cookies')}">
+
 <!ENTITY prefs.fhr.name                 "{$privacy_coach_dtd->getStringNoHTML('Firefox Health Report')}">
-<!ENTITY prefs.fhr.desc                 "{$privacy_coach_dtd->getStringNoHTML('The Firefox Health Report provides you with information about your browser’s performance and stability over time. Mozilla uses this data to provide you with meaningful comparisons and tips. We also aggregate the data shared by everyone to make Firefox better for you.')}">
+<!ENTITY prefs.fhr.pre                  "{$privacy_coach_dtd->getStringNoHTML('Firefox Health Report provides you with information about your browser’s performance and stability over time. It then gives you useful tips based on that data to help you get the most out of your browsing experience. For more information about how we handle your data, see our ')}">
+<!ENTITY prefs.fhr.link                 "{$privacy_coach_dtd->getStringNoHTML('privacy policy')}">
+<!ENTITY prefs.fhr.post                 "{$privacy_coach_dtd->getStringNoHTML('.')}">
+<!ENTITY prefs.fhr.learnMore            "{$privacy_coach_dtd->getStringNoHTML('Learn more about Firefox Health Report')}">
+
 <!ENTITY prefs.telemetry.name           "{$privacy_coach_dtd->getStringNoHTML('Telemetry')}">
-<!ENTITY prefs.telemetry.desc           "{$privacy_coach_dtd->getStringNoHTML('Telemetry shares more detailed performance, usage, hardware and customization data about your browser with Mozilla to help us make the browser better.')}">
+<!ENTITY prefs.telemetry.desc           "{$privacy_coach_dtd->getStringNoHTML('Telemetry measures and collects non-personal information about the performance of your Firefox browser — such as memory consumption, browser speed and feature usage — and sends it to Mozilla. That information is then used to help us make Firefox better for you.')}">
+<!ENTITY prefs.telemetry.learnMore      "{$privacy_coach_dtd->getStringNoHTML('Learn more about Telemetry')}">
+
 <!ENTITY prefs.crash.name               "{$privacy_coach_dtd->getStringNoHTML('Crash Reporter')}">
-<!ENTITY prefs.crash.desc               "{$privacy_coach_dtd->getStringNoHTML("The crash reporter lets you choose to submit crash reports to Mozilla to help us make the browser more stable and secure. This can help the engineers at Mozilla make your browser, but it may include some personal data, such as URLs you've visited.")}">
+<!ENTITY prefs.crash.desc               "{$privacy_coach_dtd->getStringNoHTML("The Crash Reporter gives you the opportunity to submit crash reports to Mozilla in an effort to help us make Firefox more stable and secure. A crash report may include some personal data, such as the URLs you've visited.")}">
+<!ENTITY prefs.crash.learnMore          "{$privacy_coach_dtd->getStringNoHTML('Learn more about Crash Reporter')}">
+
 <!ENTITY prefs.mls.name                 "{$privacy_coach_dtd->getStringNoHTML('Mozilla Location Services')}">
 <!ENTITY prefs.mls.desc                 "{$privacy_coach_dtd->getStringNoHTML('Mozilla Location Services shares approximate Wi-Fi and cellular location with Mozilla to help improve our geolocation service.')}">
 
-<!ENTITY features.title                 "{$privacy_coach_dtd->getStringNoHTML('Other Privacy Features')}">
-<!ENTITY features.intro                 "{$privacy_coach_dtd->getStringNoHTML('In addition to cutomizable privacy preferences, Firefox also includes features that you can use to keep your data private.')}">
-
-<!ENTITY features.learnMore             "{$privacy_coach_dtd->getStringNoHTML('Learn More')}">
+<!ENTITY features.title                 "{$privacy_coach_dtd->getStringNoHTML('Privacy Features')}">
+<!ENTITY features.intro                 "{$privacy_coach_dtd->getStringNoHTML('In addition to the customizable privacy preferences above, Firefox also includes several privacy features that you can use everyday to help keep your personal browsing data private.')}">
 
 <!ENTITY features.pb.title              "{$privacy_coach_dtd->getStringNoHTML('Private Browsing')}">
-<!ENTITY features.pb.desc.pre           "{$privacy_coach_dtd->getStringNoHTML("You can create private tabs to browse the internet without saving any information about what pages you've visited.")}">
-<!ENTITY features.pb.desc.post          "">
+<!ENTITY features.pb.desc               "{$privacy_coach_dtd->getStringNoHTML('When Private Browsing tabs are opened, Firefox for Android won’t accept cookies, remember passwords or record a history of any sites that you’ve visited. You can open a Private Browsing session by tapping on your browser menu icons and selecting &quot;New Private Tab&quot;.')}">
+<!ENTITY features.pb.learnMore          "{$privacy_coach_dtd->getStringNoHTML('Learn more about Private Browsing')}">
 
 <!ENTITY features.gb.title              "{$privacy_coach_dtd->getStringNoHTML('Guest Browsing')}">
-<!ENTITY features.gb.desc.pre           "{$privacy_coach_dtd->getStringNoHTML('A Guest Session lets someone else use Firefox on your Android device without giving them access to your personal info like passwords, bookmarks and history.')}">
-<!ENTITY features.gb.desc.post          "">
+<!ENTITY features.gb.desc               "{$privacy_coach_dtd->getStringNoHTML('A Guest Browsing session lets someone else use Firefox on your Android device without seeing your personal info like passwords, bookmarks and history.')}">
+<!ENTITY features.gb.learnMore          "{$privacy_coach_dtd->getStringNoHTML('Learn more about Guest Browsing')}">
 
-<!ENTITY features.ch.title              "{$privacy_coach_dtd->getStringNoHTML('Clear History and Other Data')}">
-<!ENTITY features.ch.desc.pre           "{$privacy_coach_dtd->getStringNoHTML('Firefox offers you control over your personal data, such as browsing history, passwords and more. You can conveniently save this data to your browser and delete it any time you want to.')}">
-<!ENTITY features.ch.desc.post          "">
+<!ENTITY features.ch.title              "{$privacy_coach_dtd->getStringNoHTML('Clear History and other data')}">
+<!ENTITY features.ch.desc               "{$privacy_coach_dtd->getStringNoHTML('Easily delete your personal data, such as browsing history, passwords and more any time you like. In your privacy settings, you can also choose to clear private data every time you select &quot;Quit&quot; from the main menu.')}">
+<!ENTITY features.ch.learnMore          "{$privacy_coach_dtd->getStringNoHTML('Learn how to clear your history')}">
 
-<!ENTITY features.ch.clearOnQuit        "{$privacy_coach_dtd->getStringNoHTML('In your privacy settings, you can also choose to clear private data every time you select &quot;Quit&quot; from the main menu.')}">
-
-<!ENTITY httpsSearch.title              "{$privacy_coach_dtd->getStringNoHTML('')}HTTPS Search">
-<!ENTITY httpsSearch.intro              "{$privacy_coach_dtd->getStringNoHTML("To keep your searches private, you should make sure to use search engines that support HTTPS. We'll prompt you before you make a search that goes over HTTP, and we'll also warn you if you try to make an HTTP search engine your default. You can change your default search engine at any time in the search settings page.")}">
-
+<!ENTITY httpsSearch.title              "{$privacy_coach_dtd->getStringNoHTML('HTTPS Search')}">
+<!ENTITY httpsSearch.intro              "{$privacy_coach_dtd->getStringNoHTML('To keep your online searches private, we recommend only using search engines that support HTTPS, a secure communications protocol. To help keep you informed, Firefox will prompt you before you make a search that goes over an unsecured HTTP network. Firefox will also give you a warning if you try to make an HTTP search engine your default. You can change your default search engine at any time in the search settings page.')}">
 
 <!ENTITY addons.title                   "{$privacy_coach_dtd->getStringNoHTML('Additional Add-ons')}">
-<!ENTITY addons.description             "{$privacy_coach_dtd->getStringNoHTML('Even though Privacy Coach takes care of a lot of privacy-related preferences, here are a few more Add-ons that can make your browsing experence even more awesome:')}">
+<!ENTITY addons.description             "{$privacy_coach_dtd->getStringNoHTML("We hope you’ve found the Firefox Privacy Coach helpful and have a better understanding of your browser's many privacy features and settings. If you would like even more control of your personal browsing data, check out some of the add-ons listed below.")}">
 
 <!ENTITY addons.ghostery.title          "{$privacy_coach_dtd->getStringNoHTML('Ghostery')}">
-<!ENTITY addons.ghostery.description    "{$privacy_coach_dtd->getStringNoHTML("Protect your privacy. See who's tracking your web browsing and block them.")}">
+<!ENTITY addons.ghostery.description    "{$privacy_coach_dtd->getStringNoHTML("Protects your privacy by letting you see who's tracking your Web browsing and block them.")}">
 <!ENTITY addons.https.title             "{$privacy_coach_dtd->getStringNoHTML('HTTPS Everywhere')}">
 <!ENTITY addons.https.description       "{$privacy_coach_dtd->getStringNoHTML('Encrypts your communications with many major websites, making your browsing more secure.')}">
 <!ENTITY addons.sdc.title               "{$privacy_coach_dtd->getStringNoHTML('Self Destructing Cookies')}">
-<!ENTITY addons.sdc.description         "{$privacy_coach_dtd->getStringNoHTML("Gets rid of a site's cookies and LocalStorage as soon as you close its tabs. Protects against trackers and zombie-cookies. Trustworthy services can be whitelisted.")}">
-<!ENTITY addons.more.pre                "{$privacy_coach_dtd->getStringNoHTML('For even more add-ons, you can explore the full set of ')}">
-<!ENTITY addons.more.link               "{$privacy_coach_dtd->getStringNoHTML('Security &amp; Privacy add-ons')}">
-<!ENTITY addons.more.post               "{$privacy_coach_dtd->getStringNoHTML('.')}">
+<!ENTITY addons.sdc.description         "{$privacy_coach_dtd->getStringNoHTML("Gets rid of a site's cookies and LocalStorage as soon as you close any open tabs for that page. It also protects against trackers and zombie-cookies and lets you whitelist services you know are trustworthy.")}">
+<!ENTITY addons.more.pre                "{$privacy_coach_dtd->getStringNoHTML('For even more, you can explore our full set of ')}">
+<!ENTITY addons.more.link               "{$privacy_coach_dtd->getStringNoHTML('Security &amp; Privacy add-ons »')}">
+<!ENTITY addons.more.post               "{$privacy_coach_dtd->getStringNoHTML('')}">
 
 <!ENTITY settingsButton.privacy         "{$privacy_coach_dtd->getStringNoHTML('Privacy Settings')}">
 <!ENTITY settingsButton.mozilla         "{$privacy_coach_dtd->getStringNoHTML('Mozilla Settings')}">
 <!ENTITY settingsButton.search          "{$privacy_coach_dtd->getStringNoHTML('Search Settings')}">
+
+<!ENTITY features.learnMore             "{$privacy_coach_dtd->getStringNoHTML('Learn More')}">
 
 TEMPLATE;
 };
