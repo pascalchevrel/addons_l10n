530ea089dd820ad265ec5fd1698a6a4192313e93
pascalc <pascal.chevrel@free.fr>
2014-10-28 17:16:52 +0100

    add zh-TW locale

--- config.php
+++ config.php
@@ -5,4 +5,4 @@ date_default_timezone_set('Europe/Paris');
 include __DIR__ . '/functions.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
-$locales = [ 'de', 'fr', 'es-ES', 'es-MX', 'hu', 'id', 'ja', 'pl', 'pt-BR', 'ru' ];
+$locales = [ 'de', 'fr', 'es-ES', 'es-MX', 'hu', 'id', 'ja', 'pl', 'pt-BR', 'ru', 'zh-TW' ];
