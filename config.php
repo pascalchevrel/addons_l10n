4f22b2c2b71889cb500b49b4855c04cb18aef9b2
pascalc <pascal.chevrel@free.fr>
2014-10-28 10:49:52 +0100

    update list of locales, remove unused array

--- config.php
+++ config.php
@@ -2,29 +2,7 @@
 
 date_default_timezone_set('Europe/Paris');
 
-include __DIR__ .'/functions.php';
+include __DIR__ . '/functions.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
-$locales = ['de', 'es-ES', 'fr', 'id', 'it', 'ja', 'pt-BR'];
-
-$addons = [
-    // 'worldcupfeed' => [
-    //     'worldcup.lang',
-    //     ['de', 'es-ES', 'fr', 'id', 'it', 'ja', 'pt-BR'],
-    //     'worldcupfeed.properties'
-    // ],
-    // 'feeds' => [
-    //     'homefeeds.lang',
-    //     ['de', 'es-ES', 'fr', 'id', 'it', 'ja', 'pt-BR'],
-    //     'feeds.properties'
-    // ],
-    'privacy-coach' => [
-        'privacy_coach.lang',
-        ['de', 'es-ES', 'fr', 'id', 'it', 'ja', 'pt-BR'],
-        'privacycoach.properties'
-    ],
-];
-
-
-// privacycoach.dtd
-
+$locales = [ 'de', 'fr', 'es-ES', 'es-MX', 'hu', 'id', 'ja', 'pl', 'pt-BR', 'ru' ];
