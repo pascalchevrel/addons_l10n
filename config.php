34abc35ee50a60831384212b9bfd456175af1428
pascalc <pascal.chevrel@free.fr>
2014-11-04 21:19:54 +0100

    Add italian

--- config.php
+++ config.php
@@ -11,4 +11,4 @@ define('SVN_LOCALES', realpath(__DIR__ . '/locales/'));
 // Path to local clone of your fork of https://github.com/leibovic/privacy-coach
 define('PRIVACY_COACH', realpath(__DIR__ . '/addons/privacy-coach/'));
 
-$locales = ['cs', 'de', 'es-ES', 'es-MX', 'fr', 'hu', 'id', 'ja', 'pl', 'pt-BR', 'ru', 'sq', 'zh-TW'];
+$locales = ['cs', 'de', 'es-ES', 'es-MX', 'fr', 'hu', 'id', 'it', 'ja', 'pl', 'pt-BR', 'ru', 'sq', 'zh-TW'];
